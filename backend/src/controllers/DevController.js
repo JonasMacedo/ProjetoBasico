59f9ad806fc37f685911cef13767578edc069969
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-01 00:44:38 -0300

    Criando metodo de inserção.

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const Dev = require('../models/Devs'); //Importando o model.
+
+module.exports ={
+    async plus(req,res){
+        
+        const {username} = req.body;
+
+        //verificando se o usuario existe no DB.
+        const userExists = await Dev.findOne({user:username});
+        if(userExists){
+            return res.json(userExists);
+        }
+
+        //Buscando o usario da api do GitHub.
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        
+        const {name,bio,avatar_url:avatar} = response.data;
+
+        //Passando dados para inserção no DB.
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar,
+        });
+
+        return res.json(dev);
+    }
+}
