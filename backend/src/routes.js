669aed115226587635103cc17dc841c14b917781
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-01 00:52:08 -0300

    Atualizando as rotas.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,12 @@
 const express = require('express');
 
 const routes = express.Router();
-
+const DevControler = require('./controllers/DevController');//Import do Controler.
 
 routes.get("/",(req,res)=>{
     return res.json({message: `Ola`});
 });
 
-routes.post("/devs",(req,res)=>{
-    
-    console.log(req.body);//Teste para apresentar o corpo da requisicao.
-
-    return res.json({ok: true});
-})
+routes.post("/devs", DevControler.plus);
 
 module.exports = routes;
