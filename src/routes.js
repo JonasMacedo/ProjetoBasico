d35e8daccddb65a01c2cf3999547d768bcc535db
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-04 15:32:41 -0300

    Adicionando rota.

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch,BrowserRouter,Route} from 'react-router-dom';
 
 import Login from './pages/Login/Login';
+import Main from './pages/Main/main';
 
 const Router = () =>{
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact path = "/" component= {Login}></Route>
+                <Route path="/main" component= {Main} ></Route>
             </Switch>
         </BrowserRouter>
     );
