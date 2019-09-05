c9c18f03e6272fa7e74ddee83895e6ea5feae214
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-05 11:33:39 -0300

    Criando pagina de Login.

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useState}from 'react';
 
 import './style.css';
 import logo from '../img/logo.svg';
+import api from '../../services/api';
 
-export default function Login(){
+export default function Login({history}){
+    
+    const [userName, setUsername] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        //Buscando usario do servidor.
+        const response = await api.post('/devs',{
+            username: userName,
+        });
+
+        const {_id} = response.data;
+
+        console.log(response.data);  
+        
+        history.push(`/devs/:${_id}`);
+    }
+
     return(
         <div className="login-container">
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <img src={logo} alt="TinDev"/>
                 <input
                     placeholder="Usuario GitHub"
-
+                    value ={userName}
+                    onChange = { e =>setUsername(e.target.value)}
                 ></input>
-                <button>Enviar</button>
+                <button type="submit">Enviar</button>
 
             </form>
         </div>
