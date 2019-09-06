d123c8b8c2f833cedf13d96b80f55a7ac0c4c2dd
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-06 20:12:21 -0300

    correção no path da rota.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,7 +9,7 @@ const Router = () =>{
         <BrowserRouter>
             <Switch>
                 <Route exact path = "/" component= {Login}></Route>
-                <Route path="/devs/:devId" component= {Main} ></Route>
+                <Route path="/devs/:id" component= {Main} ></Route>
             </Switch>
         </BrowserRouter>
     );
